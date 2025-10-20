88f8ed50e57e70a55fb16202d64f08c2a130ed59
tebogo.mohale <tebogo.mohale@mawa.co.za>
2025-10-20 20:35:42 +0200

    refactor: Remove Calculator and export ProductSearchableDropdown
    This commit removes the placeholder `Calculator` class and its associated test file, and instead, exports the `ProductSearchableDropdown` widget from the main library file.
    
    ### Changes
    - **Removal**: Deletes the `Calculator` class from `lib/mawa_ui.dart`.
    - **Removal**: Deletes the corresponding test file `test/mawa_ui_test.dart`.
    - **Export**: Adds an export statement for `ProductSearchableDropdown` in `lib/mawa_ui.dart`, making it accessible to consumers of the package.

--- lib/mawa_ui.dart
+++ lib/mawa_ui.dart
@@ -1,5 +1,3 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+library;
+
+export 'package:mawa_ui/dropdowns/product-searchable-dropdown.dart';
